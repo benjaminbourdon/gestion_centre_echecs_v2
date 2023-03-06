5ad6d2fac621d4c2c93568934fc5af66baab6297
Benjamin B <dev@benjamin-b.fr>
2023-03-06 18:34:51 +0100

    feat(player): new method to get player by id ✨ Add a get_player method returning a Player with specified federal_id

--- gce2/controller/PlayerManager.py
+++ gce2/controller/PlayerManager.py
@@ -1,11 +1,10 @@
 from gce2.model.player import Player
 from pathlib import Path
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 from gce2 import config
 
 
 class PlayerManager:
-
     def post_player(self, data: dict):
         new_player = Player.new(**data)
         return new_player
@@ -19,3 +18,15 @@ class PlayerManager:
             encoding=config.ENCODING,
         ) as json_file:
             return [Player.deserialize(player_data) for player_data in json_file.all()]
+
+    def get_player(self, federal_id):
+        path = Path(config.PATH_JSONFILE_PLAYER)
+
+        with TinyDB(
+            path,
+            indent=config.JSON_IDENT,
+            encoding=config.ENCODING,
+        ) as json_file:
+            player = Query()
+            data = json_file.search(player.federal_id == federal_id)
+            return Player.deserialize(data[0])
