387add9b552dfba829114c0c745eee755c2778e2
Benjamin B <dev@benjamin-b.fr>
2023-03-07 17:43:17 +0100

    feat : expend JSONfile connexion method ✨ New method to connect at tournaments.json file,  create it if needed

--- gce2/config.py
+++ gce2/config.py
@@ -1,3 +1,4 @@
-PATH_JSONFILE_PLAYER = 'data/players/players.json'
+PATH_JSONFILE_PLAYER = "data/players/players.json"
+PATH_JSONFILE_TOURNAMENT = "data/tournaments/tournaments.json"
 JSON_IDENT = 4
 ENCODING = "utf-8"
--- gce2/database/__init__.py
+++ gce2/database/__init__.py
@@ -5,6 +5,20 @@ from gce2 import config
 
 def get_connexion_player():
     path = Path(config.PATH_JSONFILE_PLAYER)
+    if not path.is_file():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.touch()
+
+    connexion = TinyDB(path, indent=config.JSON_IDENT, encoding=config.ENCODING)
+
+    return connexion
+
+
+def get_connexion_tournament():
+    path = Path(config.PATH_JSONFILE_TOURNAMENT)
+    if not path.is_file():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.touch()
 
     connexion = TinyDB(path, indent=config.JSON_IDENT, encoding=config.ENCODING)
 
